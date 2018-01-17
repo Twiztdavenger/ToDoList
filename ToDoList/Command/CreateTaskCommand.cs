8b381e6399a6d5265cb9378fc8cb0f6e417572a2
Matthew Daniel Muenzberg <mmuenzbe15@bw.edu>
2018-01-16 23:16:49 -0500

    Add project files.

---
+++ ToDoList/Command/CreateTaskCommand.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using ToDoList.ViewModel;
+
+namespace ToDoList.Command
+{
+    public class CreateTaskCommand : ICommand
+    {
+        public event EventHandler CanExecuteChanged;
+
+        //This makes sure we can always execute the action
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object parameter)
+        {
+            if (parameter is TaskListViewModel taskList)
+            {
+                taskList.Tasks.Add(new TaskViewModel() { Name = taskList.TaskName, Complete = false });
+            }
+        }
+    }
+}
